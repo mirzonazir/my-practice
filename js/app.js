2d99c59c5fc2c69a03aa7f123fea3421ae9601bf
mirzonazir <mirzonazir.okhunov@gmail.com>
2023-02-17 11:32:29 +0300

    update

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let myName = 'Мирзоназир';
+let myAge = 34;
+let married = true;
+
+console.log(`Моё имя ${myName}`);
+console.log(`Мне ${myAge} года`);
+console.log(`Семейное положение - ${married}`);
+
+console.log("Типы использованных переменных - " + myName + ', '+ myAge+', ' + married + ' - имеют типы ' + typeof(myName) +' '+ typeof(myAge)+' '+ typeof(married));
