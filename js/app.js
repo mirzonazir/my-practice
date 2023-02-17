40c785967aa9fa1b6e3e429be8607fef61583256
mirzonazir <mirzonazir.okhunov@gmail.com>
2023-02-17 17:46:44 +0300

    update

--- js/app.js
+++ js/app.js
@@ -9,3 +9,7 @@ console.log(`Мне ${myAge} года`);
 console.log(`Семейное положение - ${married}`);
 
 console.log("Типы использованных переменных - " + myName + ', '+ myAge+', ' + married + ' - имеют типы ' + typeof(myName) +' '+ typeof(myAge)+' '+ typeof(married));
+
+let a =false;
+a= Number(a);
+console.log(a);
